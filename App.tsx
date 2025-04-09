02c486b4ec7f9d27ebfa5d3b73e76d63c32f87c3
Guillermo Rivas <gmo.rivas@tec.mx>
2025-04-08 18:18:19 -0600

    clase de abril 8

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Pressable, Button } from 'react-native';
 import { ClassExample } from './classes/ClassExample';
 import { Doggy, DoggyRow } from './classes/DoggyComponent';
 import Requestito from './classes/RequestClass';
 import RequestFunction from './classes/RequestFunction';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 // toda la UI en react está organizada en unidades a las que llama componentes
 // core componentes y componentes nativos - definidos por react native que se traducen a UI nativa
@@ -12,7 +17,7 @@ import RequestFunction from './classes/RequestFunction';
 // 1. por medio de clases
 // 2. por medio de funciones
 
-export default function App() {
+function App() {
 
   // JSX - syntax extension for javascript
   // pseudohtml (sintácticamente)
@@ -30,7 +35,15 @@ export default function App() {
           {nombre: "Perrito3", uri: "https://www.warrenphotographic.co.uk/photography/sqrs/42790.jpg"}
         ]}
         renderItem={({item}) => {
-          return (<DoggyRow name={item.nombre} uri={item.uri} />);
+          return (
+            <Pressable 
+              onPress={()=> {
+                alert("ROW PRESIONADA!");
+              }}
+            >
+              <DoggyRow name={item.nombre} uri={item.uri} />
+            </Pressable>
+          );
         }}
       />
       <StatusBar style="auto" />
@@ -38,8 +51,77 @@ export default function App() {
   );
 }
 
+function NavRoot({navigation} : any) {
+
+  return (
+    <View style={[
+      styles.ejemploContainer,
+      {
+        flexDirection: 'column'
+      }
+    ]}>
+      {/*
+        FLEX - distribución arbitraria de espacio en pantalla
+        altura y anchura de la pantalla es igual a el total de valores flex de los elementos
+        el tamaño de cada elemento se proporciona con respecto a su valor flex y el espacio total
+      */}
+
+      {/*
+      
+        ELEMENTOS UTILIZANDO FLEXBOX
+      <Text style={{height:30, backgroundColor: 'white', flex: 2}}>DUMMY TEXT 1</Text>
+      <Text style={{height:30, backgroundColor: 'blue', flex: 3}}>HOLA A TODOS!</Text>
+      <Text style={{height:30, backgroundColor: 'pink', flex: 5}}>UN TERCER DUMMY</Text>
+        */}
+
+      <Text style={{height:30, backgroundColor: 'white'}}>DUMMY TEXT 1</Text>
+      <Text style={{height:30, backgroundColor: 'blue'}}>HOLA A TODOS!</Text>
+      <Text style={{height:30, backgroundColor: 'pink', position: 'absolute', top: 20, left:30}}>UN TERCER DUMMY</Text>
+      {/*
+      <Button 
+        title="NAVEGACION DEMO"
+        onPress={() => {
+          navigation.navigate("NavExample", {data : "ALGO DE DATOS QUE VIENEN DE FUERA!"});
+        }}
+      />
+      */}
+    </View>
+  );
+}
+
+function NavExample({navigation, route} : any) {
+
+  return (
+    <View>
+      <Text>Nav example! {route.params.data}</Text>
+    </View>
+  );
+}
+
+export default function Navigation() 
+{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="NavRoot"
+          component={NavRoot}
+        />
+        <Stack.Screen 
+          name="NavExample"
+          component={NavExample}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 // vamos a utilizar stylesheet 
 // similar a CSS
+
+// para crear un nuevo objeto con estilo utilizamos el método StyleSheet.create
+// patron de diseño de fábrica
+// https://en.wikipedia.org/wiki/Factory_method_pattern
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -47,4 +129,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  ejemploContainer: {
+    flex: 1,
+    backgroundColor: '#FFA600',
+    padding: 10,
+  }
 });
+
